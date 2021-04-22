9e828217f83b046e38e6a9e9d4ba4a8effb9ffdc
alexgerasimov58rus <vintegewhitecool@yandex.ru>
2021-04-22 18:20:29 +0300

    добавил реакцию при удалении и добавлении чата на сервере

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = express();
 server.use(cors());
 server.use(bodyParser.json());
 
-const chats = {
+let chats = {
     1: {title: 'Чат 1', input: '', isActive: false},
     2: {title: 'Чат 2', input: '', isActive: false},
     3: {title: 'Чат 3', input: '', isActive: false},
@@ -16,6 +16,7 @@ const chats = {
 let messages = {
     1: [{ text: "Привет!", author: 'Robot' }],
     2: [{ text: "Здравствуйте!", author: 'Robot' }],
+    3: []
 };
 
 const getChats = (request, response) => {
@@ -27,14 +28,52 @@ const getChatMessages= (request, response) => {
     response.status(200).send({"messages": messages[id] || [], "id": id});
 };
 
-const addChatMessage= (request, response) => {
+const addChatMessage = (request, response) => {
     const {id, author, text} = request.body;
     messages[id] = [...messages[id] || [], {"author": author, "text": text}];
-    response.status(200).send("OK");
+    response.status(200).send("add message is OK");
+};
+
+const deleteChat = (request, response) => {
+    const {id} = request.body;
+    let resChats = {};
+    let currIndex = 1;
+    Object.keys(chats).map(index => {
+        if( index !== id){
+            resChats[currIndex] = chats[index];
+            currIndex++;
+        }
+    });
+
+    chats = resChats;
+
+    let resMessages = {};
+    currIndex = 1;
+
+    Object.keys(messages).map(index => {
+        if( index !== id){
+            resMessages[currIndex] = messages[index];
+            currIndex++;
+        }
+    });
+
+    messages = resMessages;
+
+    response.status(200).send("delete chat is OK");
+};
+
+const addChat = (request, response) => {
+    const {title} = request.body;
+    const id = Object.keys(chats).length + 1;
+    chats[id]= {"title": title, "input": '', "isActive": false};
+    messages[id] = [];
+    response.status(200).send("add chat is OK");
 };
 
 server.get("/", getChats);
 server.get("/chat/:id", getChatMessages);
 server.post("/message/add", addChatMessage);
+server.post("/chat/delete", deleteChat);
+server.post("/chat/add", addChat);
 
 server.listen("8000", () => console.log("port 8000"));
--- src/store/middlewares/index.js
+++ src/store/middlewares/index.js
@@ -1,2 +1,4 @@
 export * from "./bot-send-message"
 export * from "./post-message-to-server"
+export * from "./delete-chat-from-server"
+export * from "./add-chat-to-server"
